cc38e431a4f7f3727caf643f807272b3b78d90fa
isItNeccessary <dev-venture@outlook.com>
2023-04-02 17:29:33 +0545

    feat: created a simple dice rolling game

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿ class Program
+{
+    private static void Main(string[] args)
+    {
+        Random dice = new Random();
+        int roll;
+        bool again = true;
+        string choice;
+
+        void rollDice()
+        {
+            roll = dice.Next(0, 9);
+            Console.WriteLine($"You rolled {roll}");
+
+        }
+
+        void play()
+        {
+            Console.WriteLine("Do you want to roll again? Y/N ");
+            choice = Console.ReadLine();
+            choice.ToLower();
+        }
+
+        do
+        {
+
+            rollDice();
+            play();
+            if (choice.Equals("n"))
+            {
+                again = false;
+                Console.WriteLine("Thanks for Playing");
+            }
+
+
+        } while (again == true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## **Simple Random Dice Roller**
+
+## Author: **Piyushraj Bista**
+
+## This program will continue to roll dice untill user enters 'n'
+
+### Input is case insenstive as program automatically make the input to lowercase
+
+### Requirement
+
+- .NET SDK
+
+### Instructions
+
+`dotnet run`
